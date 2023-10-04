8a5e8ccd3508088ff0adae0af29f8cede90d451a
hitesh19426 <hitesh19426@iiitd.ac.in>
2023-10-04 18:01:12 +0530

    updated application.properties and ValidationService

--- src/main/java/com/example/todolist/services/ValidationService.java
+++ src/main/java/com/example/todolist/services/ValidationService.java
@@ -3,33 +3,32 @@ package com.example.todolist.services;
 import com.example.todolist.models.Task;
 import com.example.todolist.models.TaskList;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-
+import org.springframework.util.ObjectUtils;
 import java.util.Objects;
 
 @Service
 public class ValidationService {
 
    public void validateUpdateTaskList(TaskList taskList) throws Exception {
-      if(StringUtils.isEmpty(taskList)){
+      if(ObjectUtils.isEmpty(taskList)){
          throw new Exception();
       }
    }
 
    public void validateCreateTaskRequest(String taskListId, Task task) throws Exception {
-      if(StringUtils.isEmpty(taskListId) || Objects.isNull(task) || StringUtils.isEmpty(task.getTitle())){
+      if(ObjectUtils.isEmpty(taskListId) || Objects.isNull(task) || ObjectUtils.isEmpty(task.getTitle())){
          throw new Exception();
       }
    }
 
    public void validateUpdateTaskRequest(String taskListId, Task task) throws Exception {
-      if(StringUtils.isEmpty(taskListId) || Objects.isNull(task) || StringUtils.isEmpty(task.getId())){
+      if(ObjectUtils.isEmpty(taskListId) || Objects.isNull(task) || ObjectUtils.isEmpty(task.getId())){
          throw new Exception();
       }
    }
 
    public void validateDeleteTaskRequest(String taskListId, String taskId) throws Exception {
-      if(StringUtils.isEmpty(taskListId) || StringUtils.isEmpty(taskId)){
+      if(ObjectUtils.isEmpty(taskListId) || ObjectUtils.isEmpty(taskId)){
          throw new Exception();
       }
    }
